13b1d778461b06b7194dbdddaf699b292ccde1c6
Ksenia Burova <kburova@vols.utk.edu>
2017-04-06 00:06:02 -0400

    Finished Validate()

--- project4/src/BackPropagation.java
+++ project4/src/BackPropagation.java
@@ -1,9 +1,6 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.Random;
 
 /**
@@ -13,37 +10,55 @@ import java.util.Random;
 
 public class BackPropagation {
 
+    String trainFile, validateFile, testFile;
     private int numberOfInputs = 2;
     private int numberOfLayers = 3;
     private int [] numberOfNeurons = {2, 3, 2};
     private double learningRate = 1.3;
-    private int numberOfEpochs = 200;
-    private double [][][] hiddenWeights;
-    private double [] outputWeights;
-    private double h, sigma;
-    private double [][][] deltaHidden;
-    private double [] deltaOutput;
-    private double [] input;
-    private double [][] hiddenBiasWeight;
-    private double outputBiasWeight;
+    private int numberOfEpochs = 50000;
 
-    public BackPropagation(){
+    private double [] input;
+    private double [] outputWeights;
+    private double [][][] hiddenWeights;
+    private double [][] hiddenSigma;
+    private double [][] hiddenBiasWeight;
+    private double [][] hiddenH;
+    private double [][] hiddenDelta;
+    private double outputSigma;
+    private double outputH;
+    private double outputDelta;
+    private double outputBiasWeight;
+    private double expectedOutput;
+    ArrayList <Double> RMSE;
+
+    public BackPropagation(String trainFile, String validateFile, String testFile){
+        this.trainFile = trainFile;
+        this.validateFile = validateFile;
+        this.testFile = testFile;
+
+        RMSE = new ArrayList<>();
         Random rand = new Random();
         hiddenWeights = new double[numberOfLayers][][];
-        deltaHidden = new double[numberOfLayers][][];
         outputWeights = new double[numberOfNeurons[numberOfNeurons.length-1]]; //number of nodes in last hidden layer
-        deltaOutput = new double[numberOfNeurons[numberOfNeurons.length-1]];
         input = new double[numberOfInputs];
+
         hiddenBiasWeight = new double[numberOfLayers][];
+        hiddenSigma = new double[numberOfLayers][];
+        hiddenH = new double[numberOfLayers][];
+        hiddenDelta = new double[numberOfLayers][];
+
+
 
         for (int i = 0; i < numberOfLayers; i++){
             hiddenWeights[i] = new double[ numberOfNeurons[i] ][];
-            deltaHidden[i] = new double[ numberOfNeurons[i] ][];
             hiddenBiasWeight[i] = new double[ numberOfNeurons[i]];
+            hiddenSigma[i] = new double[ numberOfNeurons[i]];
+            hiddenH[i] = new double[ numberOfNeurons[i]];
+            hiddenDelta[i] = new double[ numberOfNeurons[i]];
+
             for (int j = 0; j < numberOfNeurons[i]; j++) {
                 int numOfPrevLayerNodes = (i == 0) ? numberOfInputs : numberOfNeurons[i-1];
                 hiddenWeights[i][j] = new double[numOfPrevLayerNodes];
-                deltaHidden[i][j] = new double[numOfPrevLayerNodes];
                 hiddenBiasWeight[i][j] = (rand.nextInt((100 - (-100)) + 1) - 100) / 1000.0;;
 
                 for (int c = 0; c < numOfPrevLayerNodes; c++){
@@ -53,57 +68,153 @@ public class BackPropagation {
                     //System.out.print(weight+" ");
                 }
             }
-            outputBiasWeight = (rand.nextInt((100 - (-100)) + 1) - 100) / 1000.0;
         }
         for (int i = 0; i < numberOfNeurons[numberOfNeurons.length-1]; i++){
             outputWeights[i] = (rand.nextInt((100 - (-100)) + 1) - 100) / 1000.0;;
             //System.out.print(weight+" ");
         }
+        outputBiasWeight = (rand.nextInt((100 - (-100)) + 1) - 100) / 1000.0;
     }
 
+    private void computeOutputs(){
+        /** initialize everything to 0 **/
+        for (int i = 0; i < numberOfLayers; i++) {
+            for (int j = 0; j < numberOfNeurons[i]; j++) {
+                hiddenDelta[i][j] = 0;
+                hiddenH[i][j] = 0;
+                hiddenSigma[i][j] = 0;
+            }
+        }
+        outputDelta = 0;
+        outputH = 0;
+        outputSigma = 0;
+        /** output(h and sigma) and delta updates for hidden layers **/
+        for (int i = 0; i < numberOfLayers; i++) {
+            for (int j = 0; j < numberOfNeurons[i]; j++) {
+                if (i == 0){ // check if first layer or hidden
+                    for (int c = 0; c < numberOfInputs; c++){
+                        hiddenH[i][j] += hiddenWeights[i][j][c] * input[c];
+                    }
+                }else{
+                    for (int c = 0; c < numberOfNeurons[i - 1]; c++ ){
+                        hiddenH[i][j] += hiddenWeights[i][j][c] * hiddenSigma[i-1][c];
+                    }
+                }
+                hiddenH[i][j] += hiddenBiasWeight[i][j];
+                hiddenSigma[i][j] = 1/(1 + Math.exp(-hiddenH[i][j]));
+            } //for ( nodes/neurons )
+        } //for ( layers )
+        /** output(h and sigma) and delta updates for output **/
+        for (int j = 0; j < numberOfNeurons[numberOfNeurons.length-1]; j++){
+            outputH += hiddenSigma[numberOfLayers-1][j] * outputWeights[j];
+        }
+        outputH += outputBiasWeight;
+        outputSigma =  1/(1 + Math.exp(-outputH));
+    }
+
+    private void computeDeltaValues(){
+        outputDelta = outputSigma * (1-outputSigma) * (expectedOutput - outputSigma);
+        for (int i = numberOfLayers-1; i>=0 ; i--){
+            for (int j = 0; j < numberOfNeurons[i]; j++){
+                if (i == numberOfLayers-1){
+                    hiddenDelta[i][j] = hiddenSigma[i][j] * (1-hiddenSigma[i][j]) * outputDelta * outputWeights[j];
+                }else{
+                    for (int c = 0; c < numberOfNeurons[i+1]; c++){
+                        hiddenDelta[i][j] += hiddenSigma[i][j] * (1 - hiddenSigma[i][j]) * hiddenDelta[i+1][c] * hiddenWeights[i+1][c][j];
+                    }
+                }
+            }
+        }
+    }
+    private void updateWeights(){
+        /** hidden layers **/
+        for (int i = 0; i < numberOfLayers; i++) {
+            for (int j = 0; j < numberOfNeurons[i]; j++) {
+                if (i == 0){
+                    for (int c = 0; c < numberOfInputs; c++){
+                        hiddenWeights[i][j][c] += learningRate * hiddenDelta[i][j] * input[c];
+                    }
+                }else{
+                    for (int c = 0; c < numberOfNeurons[i - 1]; c++ ){
+                        hiddenWeights[i][j][c] += learningRate * hiddenDelta[i][j] * hiddenSigma[i-1][c];
+                    }
+                }
+                hiddenBiasWeight[i][j] += learningRate * hiddenDelta[i][j];
+            } //for ( nodes/neurons )
+        } //for ( layers )
+        /** output node **/
+        for (int j = 0; j < numberOfNeurons[numberOfNeurons.length-1]; j++){
+            outputWeights[j] += learningRate * outputDelta * hiddenSigma[numberOfLayers-1][j];
+        }
+        outputBiasWeight += learningRate * outputDelta;
+    }
     public void trainNetwork() {
         try {
-            BufferedReader br = new BufferedReader(new FileReader("problem1/training1.txt"));
-
-            /** initialize everything to 0 **/
-            sigma = 0;
-            h = 0;
-            for (int i = 0; i < numberOfLayers; i++) {
-                for (int j = 0; j < numberOfNeurons[i]; j++) {
-                    int numOfPrevLayerNodes = (i == 0) ? numberOfInputs : numberOfNeurons[i - 1];
-                    for (int c = 0; c < numOfPrevLayerNodes; c++) {
-                        deltaHidden[i][j][c] = 0;
-                    }
+            String line;
+            BufferedReader br = new BufferedReader(new FileReader(trainFile));
+            while ( (line = br.readLine()) != null ) {
+                String [] vals = line.split(" ");
+                for (int i = 0; i < numberOfInputs; i++){
+                    input[i] = Double.valueOf(vals[i]);
                 }
-            }
-            for (int i = 0; i < numberOfNeurons[numberOfNeurons.length - 1]; i++) {
-                deltaOutput[i] = 0;
-            }
+                expectedOutput = Double.valueOf(vals[vals.length-1]);
 
-            for (int i = 0; i < numberOfLayers; i++) {
-                for (int j = 0; j < numberOfNeurons[i]; j++) {
-                    if (i == 0){
-                        for (int c = 0; c < numberOfInputs; c++){
-                            h += hiddenWeights[i][j][c] * input[c];
-                        }
-                    }else{
-                        for (int c = 0; c < numberOfNeurons[i - 1]; c++ ){
-                            h += hiddenWeights[i][j][c] /* * sigma */;
-                        }
-                    }
-                }
+                computeOutputs();
+                computeDeltaValues();
+                updateWeights();
             }
+            br.close();
         }catch (IOException e){
             e.printStackTrace();
         }
     }
-    public void validateNetwork(int epoch) {}
+    public void validateNetwork() {
+        double sum = 0.0;
+        int numOfPatterns = 0;
+        double rmse;
+        try{
+            String line;
+            BufferedReader br = new BufferedReader(new FileReader(trainFile));
+            while ( (line = br.readLine()) != null ) {
+                String[] vals = line.split(" ");
+                for (int i = 0; i < numberOfInputs; i++){
+                    input[i] = Double.valueOf(vals[i]);
+                }
+                expectedOutput = Double.valueOf(vals[vals.length-1]);
+                computeOutputs();
+                sum += Math.pow((expectedOutput - outputSigma),2);
+                numOfPatterns++;
+            }
+            rmse = Math.sqrt(sum/(2.0*numOfPatterns));
+            RMSE.add(rmse);;
+            br.close();
+        }catch (IOException e){
+            e.getStackTrace();
+        }
+    }
+
+    private void printRMSE(){
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter("report.csv"));
+            bw.write("Project 4, CS420\n Artificial, Neural Nets\nCompleted by:, Ksenia Burova\n\n\n");
+            bw.write("Epoch #, RMSE\n");
+            for (int i = 0; i < RMSE.size(); i++) {
+                bw.write(i + "," + RMSE.get(i) + "\n");
+            }
+            bw.flush();
+            bw.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
     public void testNetwork() {}
 
     public void run(){
         for (int i = 0; i < numberOfEpochs; i++){
+            System.out.println(i);
             trainNetwork();
-            validateNetwork(i);
+            validateNetwork();
+            printRMSE();
         }
         testNetwork();
     }
@@ -111,7 +222,7 @@ public class BackPropagation {
         if (args.length != 3){
             System.out.println("Usage: java BackPropagation [train.txt] [validate.txt] [test.txt] ");
         }
-        BackPropagation bp = new BackPropagation();
+        BackPropagation bp = new BackPropagation(args[0],args[1],args[2]);
         bp.run();
     }
 }
